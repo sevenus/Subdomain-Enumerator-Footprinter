54900a4233846e1979e5d41d57630484a1972d16
sevenus <aaron.stracke@gmail.com>
2018-09-24 15:38:27 -0400

    Updated, working with CNAME record pulls
    -removed broken error handling
    -included CNAME output cleanup for further processing
    -included additional processing printout for troubleshooting

--- CNAME_test_with_open2.py
+++ CNAME_test_with_open2.py
@@ -1,68 +1,94 @@
 from dns import resolver
 import dns
 import os
+import string
 import csv
+
 errcountA = 0
 errcountCNAME = 0
 errcountCNAME2 = 0
+errcountNS = 0
+errcountSOA = 0
+errcountSOA2 = 0
+
 list = []
 combolist = []
-with open ("c:/temp/sublist.txt") as sublist:
-    for url in sublist:
-        A = []
-        CNAME = []
-        CNAME2 = []
-        url = url.strip()
-        try:
-            answer = resolver.query(url, 'A')
-            for rdata in answer:
-                A.append(rdata.address)
-        except:
-            A = "no IPV4 found"
-            errcountA += 1
-            continue 
-        try:
-            answer = resolver.query(url, 'CNAME')
-            for rdata in answer:
-                CNAME.append(rdata.target)
-        except:
-            CNAME = "no CNAME found"
-            errcountCNAME += 1
-            continue
-        try:
-        	answer = resolver.query(CNAME, 'CNAME')
-        	for rdata in answer:
-        		CNAME2.append(rdata.target)
-        except:
-        	CNAME2 = "no nested CNAMEs"
-        	errcountCNAME2 += 1
-            continue
-        finally:
-            list = [url, A, CNAME, CNAME2]
-            #list = zip(url,A,CNAME)
-            combolist.append(list)
 
+sublist1 = open ("c:/temp/sublist2.txt", "r").read().replace('\r', '').splitlines()
+
+print "\r\n"
+print "List of URLs to be Resolved: "
+print "\r\n"
+print sublist1
+print "\r\n"
+print "there are %d total URLs in this list" %len(sublist1)
+print "\r\n"
+print "URLs being processed:"
+
+for url in sublist1:
+    A = []
+    CNAME = []
+    SOA = []
+    SOA2 = []
+    print "read %s prior to Resolver" %url.split()
+    
+    try:
+        answer = dns.resolver.query(url, 'A', raise_on_no_answer=True)
+        for rdata in answer:
+            print rdata
+            A.append(rdata.address) #(rdata.address)
+            print type(A[0])
+            print A
+            #time.sleep(1)
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
+        A = "IPv4 not found"
+        errcountA += 1
+    
+    try:
+        answer = dns.resolver.query(url, 'CNAME')
+        for rdata in answer:
+            print rdata
+            CNAME.append(rdata.target)
+            CNAME = str(CNAME).lstrip("[<DNS name ").rstrip(".>]")
+            print CNAME
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
+        CNAME = "no CNAME found"
+        errcountCNAME += 1
+    
+    try:
+     	answer = dns.resolver.query(url, 'SOA')
+        for rdata in answer:
+            print rdata
+            SOA.append(rdata.mname)
+            SO2.append(rdata.rname)
+            print SOA, SOA2
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
+        SOA = "no SOA found"
+        SOA2 = "no SOA found"
+        errcountSOA2 += 1
+
+    list = [url, A, CNAME, SOA, SOA2]
+    combolist.append(list)
+    print "read %s after Resolver" %url
+                
 with open("c:/temp/subsresolvedtest.csv", 'wb') as csvfile:
     writer = csv.writer(csvfile)
     for r in combolist:
         writer.writerow(r)
+
 print "\r\n"
-print A
+print "printing CNAME list:"
 print "\r\n"
 print CNAME
 print "\r\n"
-print url
-print "\r\n"
-print list
+print "URLs Resolved:"
 print "\r\n"
 print combolist
 print "\r\n"
-print ("There were %d A record errors") %errcountA
+print ("There were %d 'A' record errors") %errcountA
+print "\r\n"
 print ("There were %d CNAME record errors") %errcountCNAME
 print "\r\n"
-print ("%d CNAME records are not nested") %errcountCNAME2
+print ("There were %d nested SOA record errors") %errcountSOA
 print "\r\n"
 print ("Done")
-
-
-
